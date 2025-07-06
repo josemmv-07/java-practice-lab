796e19122481fd6bc33af0651acce798c102c384
josemmv <josemanuelmorenovalenzuela@gmail.com>
2025-07-06 13:48:35 +0200

    Variable probando

---
+++ src/UsoVariable.java
@@ -0,0 +1,20 @@
+
+public class UsoVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte edad; // Estamos indicando lo que es la variable 
+		
+		edad=17; 
+		/*
+		 * podemos poner el valor donde queramos a noser que 
+		 * hagamos var edad=17; tiene que esta en la misma linea por que es esto es inferencia de tipos
+		 */
+		edad=34;
+		
+		System.out.println(edad);
+
+	}
+
+}
